8447b658e233592f5ee3bcf24017124eb07e748e
Joe Webb <49792657+WebbDeveloping@users.noreply.github.com>
2022-08-30 09:56:41 -0600

    Added profile page and some styles to it

--- src/components/profile/ProfilePage.js
+++ src/components/profile/ProfilePage.js
@@ -2,6 +2,35 @@ import React from 'react'
 
 export default function ProfilePage() {
   return (
-    <div>ProfilePage</div>
+    <div className="profile-page">
+    <div className="grid">
+      <div className="row flex">
+        <div className="module col-2-of-3">
+          <div className="profile-page__image-banner">
+            <img 
+            src="https://assets.website-files.com/6260849a6eab2a733e282630/627428633aafc4d117e3638d_profile-bg-banner-small-dashboardly-webflow-template-p-800.jpeg" 
+            alt="" 
+            className="image fit-cover" />
+          </div>
+          <div className="profile-page__bio-container">
+            <div className="flex--align-end">
+              <div className="profile-page__profile-picture-container">
+                <img src="https://assets.website-files.com/6260849a6eab2a733e282630/626997720dc9d4061a009219_john-carter-avatar-medium-dashboardly-webflow-template.jpg" alt="" className="avatar-circle" />
+              </div>
+              <div className="profile-page__name-container"></div>
+            </div>
+            <div className="divider"/>
+            <div className="profile-page__about-right-text">
+              <h4 className="h4 mb-32">About Me</h4>
+              <p className="text__200 mb-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <p className="text__200 mb-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <p className="text__200 mb-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+          </div>
+        </div>
+        <div className="module col-1-of-3">1-4</div>
+      </div>
+    </div>
+    </div>
   )
 }
--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -40,7 +40,7 @@ export default function Pricing() {
                 <p className="text__200--bold mb-20">
                   <span className="text__200 ">Features Include</span>
                 </p>
-                <div className="grid mb-16">
+                <div className="mb-16">
                   <div className="row">
                     <div className="mb-24 grid-1-of-1">
                       <div className="flex mb-16">
@@ -67,13 +67,58 @@ export default function Pricing() {
               </a>
             </div>
           </div>
+          <div className="col-1-of-3 featured h-100">
+            <div className="h-100 stretch">
+              <a href="/Pricing" className="module featured price-card inline-block">
+                <div className="price-card__title-wrapper">
+                  <h2 className="h6 mb-16">Professional</h2>
+                  <div className="flex__align-end mb-8">
+                    <p className="h2 text__400--bold yearly">$299</p>
+                    {/* <p className="h2 monthly">$19</p> */}
+                    <p className="text__100 mb-8 yearly">/year</p>
+                    {/* <p className="text-600 mb-8 monthly">/month</p> */}
+                  </div>
+                </div>
+                <p className="text__100 mb-32">Lorem ipsum dolor sit amete consectetur adipiscing elit tempor</p>
+                <div className="btn btn--primary mb-20">Select Plan</div>
+                <div className="divider__32"></div>
+                <p className="text__200--bold mb-20">
+                  <span className="text__200 ">Features Include</span>
+                </p>
+                <div className="mb-16">
+                  <div className="row">
+                    <div className="mb-24 grid-1-of-1">
+                      <div className="flex mb-16">
+                        <img src="https://assets.website-files.com/6260849a6eab2a733e282630/627568ec6814f96e102f6cac_pricing-v1-checkmark-gray-dashboardly-webflow-template.svg" alt="" className="mr-16" />
+                        <p className="text__200">All analytic features</p>
+                      </div>
+                      <div className="flex mb-16">
+                        <img src="https://assets.website-files.com/6260849a6eab2a733e282630/627568ec6814f96e102f6cac_pricing-v1-checkmark-gray-dashboardly-webflow-template.svg" alt="" className="mr-16" />
+                        <p className="text__200">All analytic features</p>
+                      </div>
+                      <div className="flex mb-16">
+                        <img src="https://assets.website-files.com/6260849a6eab2a733e282630/627568ec6814f96e102f6cac_pricing-v1-checkmark-gray-dashboardly-webflow-template.svg" alt="" className="mr-16" />
+                        <p className="text__200">All analytic features</p>
+                      </div>
+                      <div className="flex mb-16">
+                        <img src="https://assets.website-files.com/6260849a6eab2a733e282630/627568ec6814f96e102f6cac_pricing-v1-checkmark-gray-dashboardly-webflow-template.svg" alt="" className="mr-16" />
+                        <p className="text__200">All analytic features</p>
+                      </div>
+                      
+                    </div>
+                  </div>
+                </div>
+                <div className="btn btn--primary mb-20">Select Plan</div>
+              </a>
+            </div>
+          </div>
           <div className="col-1-of-3 h-100">
             <div className="h-100 stretch">
               <a href="/Pricing" className="module price-card inline-block">
                 <div className="price-card__title-wrapper">
-                  <h2 className="h6 mb-16">Start Up</h2>
+                  <h2 className="h6 mb-16">Enterprise</h2>
                   <div className="flex__align-end mb-8">
-                    <p className="h2 text__400--bold yearly">$199</p>
+                    <p className="h2 text__400--bold yearly">$399</p>
                     {/* <p className="h2 monthly">$19</p> */}
                     <p className="text__100 mb-8 yearly">/year</p>
                     {/* <p className="text-600 mb-8 monthly">/month</p> */}
@@ -85,7 +130,7 @@ export default function Pricing() {
                 <p className="text__200--bold mb-20">
                   <span className="text__200 ">Features Include</span>
                 </p>
-                <div className="grid mb-16">
+                <div className="mb-16">
                   <div className="row">
                     <div className="mb-24 grid-1-of-1">
                       <div className="flex mb-16">
@@ -112,54 +157,6 @@ export default function Pricing() {
               </a>
             </div>
           </div>
-          <div className="col-1-of-3 h-100">
-            <div className="h-100 stretch">
-              <a href="/Pricing" className="module price-card inline-block">
-                <div className="price-card__title-wrapper">
-                  <h2 className="h6 mb-16">Start Up</h2>
-                  <div className="flex__align-end mb-8">
-                    <p className="h2 text__400--bold yearly">$199</p>
-                    {/* <p className="h2 monthly">$19</p> */}
-                    <p className="text__100 mb-8 yearly">/year</p>
-                    {/* <p className="text-600 mb-8 monthly">/month</p> */}
-                  </div>
-                </div>
-                <p className="text__100 mb-32">Lorem ipsum dolor sit amete consectetur adipiscing elit tempor</p>
-                <div className="btn btn--secondary mb-20">Select Plan</div>
-                <div className="divider__32"></div>
-                <p className="text__200--bold mb-20">
-                  <span className="text__200 ">Features Include</span>
-                </p>
-                <div className="grid mb-16">
-                  <div className="row">
-                    <div className="mb-24 grid-1-of-1">
-                      <div className="flex mb-16">
-                        <img src="https://assets.website-files.com/6260849a6eab2a733e282630/627568ec6814f96e102f6cac_pricing-v1-checkmark-gray-dashboardly-webflow-template.svg" alt="" className="mr-16" />
-                        <p className="text__200">All analytic features</p>
-                      </div>
-                      <div className="flex mb-16">
-                        <img src="https://assets.website-files.com/6260849a6eab2a733e282630/627568ec6814f96e102f6cac_pricing-v1-checkmark-gray-dashboardly-webflow-template.svg" alt="" className="mr-16" />
-                        <p className="text__200">All analytic features</p>
-                      </div>
-                      <div className="flex mb-16">
-                        <img src="https://assets.website-files.com/6260849a6eab2a733e282630/627568ec6814f96e102f6cac_pricing-v1-checkmark-gray-dashboardly-webflow-template.svg" alt="" className="mr-16" />
-                        <p className="text__200">All analytic features</p>
-                      </div>
-                      <div className="flex mb-16">
-                        <img src="https://assets.website-files.com/6260849a6eab2a733e282630/627568ec6814f96e102f6cac_pricing-v1-checkmark-gray-dashboardly-webflow-template.svg" alt="" className="mr-16" />
-                        <p className="text__200">All analytic features</p>
-                      </div>
-                      
-                    </div>
-                  </div>
-                </div>
-                <div className="btn btn--secondary mb-20">Select Plan</div>
-              </a>
-            </div>
-          </div>
-
-
-
         </div>
       </div>
     </div>
